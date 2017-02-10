7b1a4010009166102a9382b3ebc6019fc9d7b18a
Petr Balok <petrbalok@gmail.com>
2017-02-10 10:28:48 +0100

    Upraven cyklus na skript, co počítá do 1 000 000.

--- cyklus.sh
+++ cyklus.sh
@@ -1,1 +1,5 @@
-jjjjjjjjjjjjjjjjjjjjjjjjaaaaaaaaaaaaaajjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjlllllllllllllllllllllllllllllllllllllllll
+#!/bin/bash
+for (( i=1; i<=1000000; i++ ))
+do
+echo $i
+done
