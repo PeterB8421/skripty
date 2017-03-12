6d37fb1253ce1b36fb755101fb8b4b332e56b573
PeterB_8421 <petrbalok@gmail.com>
2017-03-12 13:11:16 +0100

    Dokončen skript pro úpravu obrázků z terminálu.

--- ImgEdit.sh
+++ ImgEdit.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 choice=55
 until [ $choice == e ]; do
-	clear
 	echo "__________Welcome to image editor, choose what you want to do: __________"
 	echo "|||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||"
 	echo "|									      |"
@@ -15,7 +14,9 @@ until [ $choice == e ]; do
 	echo "|||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||"
 	echo "Your choice: "
 	read choice
+	clear
 	if [ $choice == 1 ]; then
+		echo "RESOLUTION"
 		echo "Write path to your image (if you want to change all images, write *.[format]): "
 		read path
 		echo "Write new resolution: "
@@ -36,15 +37,48 @@ until [ $choice == e ]; do
 		echo "Write path to your image (if you want to change all images, write *.[format]): "
 		read path
 		if [ $choice == c ]; then
-			echo "Write nuber of strength of the charcoal effect: "
+			echo "Write number of strength of the charcoal effect: "
 			read strength
-			convert $path -charcoal $strength charcoal-$path
+			convert $path -charcoal $strength $path
 			echo "Charcoal effect applied!"
 		elif [ $choice == i ]; then
-			echo "Write nuber of strength of the charcoal effect: "
+			echo "Write number of strength of the implosion effect: "
 			read strength
-			convert $path -implode $strength implosion-$path
+			convert $path -implode $strength $path
 			echo "Implosion (black hole) effect applied!"
 		fi
+	elif [ $choice == 3 ]; then
+		echo "Format MENU"
+		echo "(1) png -> jpg"
+		echo "(2) jpg -> png"
+		echo "--------------"
+		echo "(b) Back"
+		read choice
+		echo "Write path to your image (if you want to change all images, write *.[format]): "
+		read path
+		if [ $choice == 1 ]; then
+			echo "Enter quality of converted image: "
+			read quality
+			convert $path -quality $quality $path.jpg
+		elif [ $choice == 2 ]; then
+			convert $path $path.png
+		fi
+		echo "Image converted!"
+	elif [ $choice == 4 ]; then
+		echo "ROTATE"
+		echo "Write path to your image (if you want to change all images, write *.[format]): "
+		read path
+		echo "Write number of degrees to rotate the image: "
+		read rotate
+		convert $path -rotate $rotate $path
+		echo "Image rotated!"
+	elif [ $choice == 5 ]; then
+		echo "QUALITY"
+		echo "Write path to your image (if you want to change all images, write *.[format]): "
+		read path
+		echo "Write number for quality in the image: "
+		read quality
+		convert $path -quality $quality $path
+		echo "Quality changed!"
 	fi
 done
